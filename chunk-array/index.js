fce8bb9a38a7e45ecbc08ae354a5b2a067b1eb01
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2022-02-08 17:24:03 +0100

    add chunk array exercise

---
+++ chunk-array/index.js
@@ -0,0 +1,10 @@
+function chunkArray(arr, size) {
+    var newArr = [];
+  
+    for (let i = 0; i < arr.length; i+=size)
+    newArr.push(arr.slice(i, i + size));
+    
+    return newArr;
+  }
+  console.log(chunkArray(["a", "b", "c", "d"], 1));
+  
